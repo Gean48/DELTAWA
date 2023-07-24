1ed4c7903f0b757c211111a36ed89d030d94291c
Gean48 <61244326+Gean48@users.noreply.github.com>
2023-07-24 10:49:51 -0300

    Update and rename WhatsAppBeta.txt to DELTAAF.js

---
+++ DELTAAF.js
@@ -0,0 +1,10 @@
+{
+	"version": "1.21",
+	"download": "https://ymmods.net/",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/Gean48/WhatsAppPrivacy/main/WACHANGELOG.html"
+		}
+	]
+}
