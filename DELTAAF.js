15dc1c6acd80699d950bd38c3f5f8911f1a7f781
Gean48 <61244326+Gean48@users.noreply.github.com>
2023-07-24 10:59:46 -0300

    Update DELTAAF.js

--- DELTAAF.js
+++ DELTAAF.js
@@ -4,7 +4,7 @@
 	"update": [
 		{
 			"languaje": "en",
-			"changelog": "https://raw.githubusercontent.com/Gean48/WhatsAppBeta/main/DELTACHANGELOG.html"
+			"changelog": "https://raw.githubusercontent.com/Gean48/DELTAWA/main/DELTACHANGELOG.html"
 		}
 	]
 }
